b041c2cfd05b3d1a7fd0405c8a6d190eab687a4e
Tellow Krinkle <tellowkrinkle@gmail.com>
2020-06-11 23:48:58 -0500

    Add e**d registers for 32-bit addressing
    Note: for transition only, will replace e** registers in the future

--- common/include/x86emitter/x86types.h
+++ common/include/x86emitter/x86types.h
@@ -640,6 +640,13 @@ extern const xAddressReg
     eax, ebx, ecx, edx,
     esi, edi, ebp, esp;
 
+// Temporary registers to aid the move to x86-64
+extern const xRegister32
+    eaxd, ebxd, ecxd, edxd,
+    esid, edid, ebpd, espd,
+     r8d,  r9d, r10d, r11d,
+    r12d, r13d, r14d, r15d;
+
 extern const xRegister16
     ax, bx, cx, dx,
     si, di, bp, sp;
@@ -931,6 +938,7 @@ protected:
     void Reduce();
 };
 
+typedef xIndirect<uptr> xIndirectNative;
 typedef xIndirect<u128> xIndirect128;
 typedef xIndirect<u64> xIndirect64;
 typedef xIndirect<u32> xIndirect32;
@@ -964,6 +972,11 @@ public:
         m_OpSize = src.GetOperandSize();
     }
     xIndirect64orLess(const xIndirect64 &src)
+        : _parent(src)
+    {
+        m_OpSize = src.GetOperandSize();
+    }
+    xIndirect64orLess(const xIndirectNative &src)
         : _parent(src)
     {
         m_OpSize = src.GetOperandSize();
@@ -1017,6 +1030,7 @@ public:
 // ptr[] - use this form for instructions which can resolve the address operand size from
 // the other register operand sizes.
 extern const xAddressIndexer<xIndirectVoid> ptr;
+extern const xAddressIndexer<xIndirectNative> ptrNative;
 extern const xAddressIndexer<xIndirect128> ptr128;
 extern const xAddressIndexer<xIndirect64> ptr64;
 extern const xAddressIndexer<xIndirect32> ptr32;
