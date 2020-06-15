98ac8605652f25f32adfeaf1e823e3624c724bac
Tellow Krinkle <tellowkrinkle@gmail.com>
2020-06-15 00:55:11 -0500

    iR5900AritImm.cpp x86-64

--- common/include/x86emitter/x86types.h
+++ common/include/x86emitter/x86types.h
@@ -482,7 +482,7 @@ public:
 #else
 #define xRegisterLong xRegister32
 #endif
-const int wordsize = sizeof(sptr);
+static const int wordsize = sizeof(sptr);
 
 class xAddressReg : public xRegisterLong
 {
