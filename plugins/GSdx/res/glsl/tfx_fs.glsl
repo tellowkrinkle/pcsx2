8161ad984944aeb8095bb598f62c3d38a9284e01
Tellow Krinkle <tellowkrinkle@gmail.com>
2020-06-29 20:41:16 -0500

    Compatibility with GL missing defines for ARB_enhanced_layouts

--- plugins/GSdx/res/glsl/tfx_fs.glsl
+++ plugins/GSdx/res/glsl/tfx_fs.glsl
@@ -26,7 +26,7 @@
 
 #ifdef FRAGMENT_SHADER
 
-#if !defined(BROKEN_DRIVER) && defined(GL_ARB_enhanced_layouts) && GL_ARB_enhanced_layouts
+#if !defined(BROKEN_DRIVER) && HAS_ENHANCED_LAYOUTS
 layout(location = 0)
 #endif
 in SHADER
