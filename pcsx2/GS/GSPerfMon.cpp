33c3a9747b5f210253244e4915e52451ef13fbed
TellowKrinkle <tellowkrinkle@gmail.com>
2020-08-08 15:01:29 -0500

    Use correct clock for GSdx fps calculations on unix

--- pcsx2/GS/GSPerfMon.cpp
+++ pcsx2/GS/GSPerfMon.cpp
@@ -35,9 +35,13 @@ void GSPerfMon::Put(counter_t c, double val)
 	{
 #if defined(__unix__) || defined(__APPLE__)
 		// clock on linux will return CLOCK_PROCESS_CPUTIME_ID.
-		// CLOCK_THREAD_CPUTIME_ID is much more useful to measure the fps
+		// we want actual time, not CPU time
 		struct timespec ts;
-		clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
+# ifdef CLOCK_MONOTONIC_RAW
+		clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+# else
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+# endif
 		uint64 now = (uint64)ts.tv_sec * (uint64)1e6 + (uint64)ts.tv_nsec / (uint64)1e3;
 #else
 		clock_t now = clock();
