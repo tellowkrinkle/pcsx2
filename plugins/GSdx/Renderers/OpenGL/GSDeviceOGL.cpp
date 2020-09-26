483b99430feda3bb863be7f84cf58899d4cd5342
TellowKrinkle <tellowkrinkle@gmail.com>
2020-09-26 03:59:18 -0500

    GSdx: Rename GL convert shaders

--- plugins/GSdx/Renderers/OpenGL/GSDeviceOGL.cpp
+++ plugins/GSdx/Renderers/OpenGL/GSDeviceOGL.cpp
@@ -397,8 +397,10 @@ bool GSDeviceOGL::Create(const std::shared_ptr<GSWnd> &wnd)
 
 		m_convert.vs = vs;
 		for(size_t i = 0; i < countof(m_convert.ps); i++) {
-			ps = m_shader->Compile("convert.glsl", format("ps_main%d", i), GL_FRAGMENT_SHADER, shader.data());
-			std::string pretty_name = "Convert pipe " + std::to_string(i);
+			const char* name = shaderName((ShaderConvert)i);
+
+			ps = m_shader->Compile("convert.glsl", name, GL_FRAGMENT_SHADER, shader.data());
+			std::string pretty_name = std::string("Convert pipe ") + name;
 			m_convert.ps[i] = m_shader->LinkPipeline(pretty_name, vs, 0, ps);
 		}
 
