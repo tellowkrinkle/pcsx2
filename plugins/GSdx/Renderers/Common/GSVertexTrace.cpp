529358608fbd285d25fd57e4a1f1304931a904cb
TellowKrinkle <tellowkrinkle@gmail.com>
2020-11-20 22:01:25 -0600

    gsdx: Make GSVertexTrace::FindMinMax scan vertices instead of primitives where possible

--- plugins/GSdx/Renderers/Common/GSVertexTrace.cpp
+++ plugins/GSdx/Renderers/Common/GSVertexTrace.cpp
@@ -68,7 +68,7 @@ void GSVertexTrace::Update(const void* vertex, const uint32* index, int v_count,
 	uint32 fst = m_state->PRIM->FST;
 	uint32 color = !(m_state->PRIM->TME && m_state->m_context->TEX0.TFX == TFX_DECAL && m_state->m_context->TEX0.TCC);
 
-	(this->*m_fmm[m_accurate_stq][color][fst][tme][iip][primclass])(vertex, index, i_count);
+	(this->*m_fmm[m_accurate_stq][color][fst][tme][iip][primclass])(vertex, index, v_count, i_count);
 
 	// Potential float overflow detected. Better uses the slower division instead
 	// Note: If Q is too big, 1/Q will end up as 0. 1e30 is a random number
@@ -76,7 +76,7 @@ void GSVertexTrace::Update(const void* vertex, const uint32* index, int v_count,
 	if (!fst && !m_accurate_stq && m_min.t.z > 1e30) {
 		fprintf(stderr, "Vertex Trace: float overflow detected ! min %e max %e\n", m_min.t.z, m_max.t.z);
 		m_accurate_stq = true;
-		(this->*m_fmm[m_accurate_stq][color][fst][tme][iip][primclass])(vertex, index, i_count);
+		(this->*m_fmm[m_accurate_stq][color][fst][tme][iip][primclass])(vertex, index, v_count, i_count);
 	}
 
 	m_eq.value = (m_min.c == m_max.c).mask() | ((m_min.p == m_max.p).mask() << 16) | ((m_min.t == m_max.t).mask() << 20);
@@ -155,7 +155,7 @@ void GSVertexTrace::Update(const void* vertex, const uint32* index, int v_count,
 }
 
 template<GS_PRIM_CLASS primclass, uint32 iip, uint32 tme, uint32 fst, uint32 color, uint32 accurate_stq>
-void GSVertexTrace::FindMinMax(const void* vertex, const uint32* index, int count)
+void GSVertexTrace::FindMinMax(const void* vertex, const uint32* index, int v_count, int i_count)
 {
 	const GSDrawingContext* context = m_state->m_context;
 
@@ -194,348 +194,194 @@ void GSVertexTrace::FindMinMax(const void* vertex, const uint32* index, int coun
 
 	const GSVertex* RESTRICT v = (GSVertex*)vertex;
 
-	for(int i = 0; i < count; i += n)
+	for(int i = 0; i < (v_count - 1); i += 2) // 2x loop unroll
 	{
-		if(primclass == GS_POINT_CLASS)
-		{
-			GSVector4i c(v[index[i]].m[0]);
+		const GSVertex& v0 = v[i + 0];
+		const GSVertex& v1 = v[i + 1];
 
-			if(color)
+		if (color)
+		{
+			GSVector4i c0 = GSVector4i::load(v0.RGBAQ.u32[0]);
+			GSVector4i c1 = GSVector4i::load(v1.RGBAQ.u32[0]);
+			if (iip || primclass == GS_POINT_CLASS)
+			{
+				cmin = cmin.min_u8(c0.min_u8(c1));
+				cmax = cmax.max_u8(c0.max_u8(c1));
+			}
+			else if (primclass == GS_SPRITE_CLASS)
+			{
+				// Point and sprite always have indices == vertices
+				// Other classes will need to use the index table here
+				cmin = cmin.min_u8(c1);
+				cmax = cmax.max_u8(c1);
+			}
+		}
+
+		if (tme)
+		{
+			if (!fst)
+			{
+				GSVector4 stq0 = GSVector4::cast(GSVector4i(v0.m[0]));
+				GSVector4 stq1 = GSVector4::cast(GSVector4i(v1.m[0]));
+
+				GSVector4 st, q;
+				// Sprites always have indices == vertices, so we don't have to look at the index table here
+				if (primclass == GS_SPRITE_CLASS)
+					q = stq1.wwww();
+				else
+					q = stq0.wwww(stq1);
+
+				if (accurate_stq)
+					st = stq0.xyxy(stq1) / q;
+				else
+					st = stq0.xyxy(stq1) * q.rcpnr();
+
+				stq0 = st.xyww(primclass == GS_SPRITE_CLASS ? stq1 : stq0);
+				stq1 = st.zwww(stq1);
+
+				tmin = tmin.min(stq0.min(stq1));
+				tmax = tmax.max(stq0.max(stq1));
+			}
+			else
+			{
+				GSVector4i uv0(v0.m[1]);
+				GSVector4i uv1(v1.m[1]);
+
+				GSVector4 st0 = GSVector4(uv0.uph16()).xyxy();
+				GSVector4 st1 = GSVector4(uv1.uph16()).xyxy();
+
+				tmin = tmin.min(st0.min(st1));
+				tmax = tmax.max(st0.max(st1));
+			}
+		}
+
+		GSVector4i xyzf0(v0.m[1]);
+		GSVector4i xyzf1(v1.m[1]);
+
+		GSVector4i xy0 = xyzf0.upl16();
+		GSVector4i z0 = xyzf0.yyyy();
+		GSVector4i xy1 = xyzf1.upl16();
+		GSVector4i z1 = xyzf1.yyyy();
+
+		#if _M_SSE >= 0x401
+
+		GSVector4i p0 = xy0.blend16<0xf0>(z0.uph32(primclass == GS_SPRITE_CLASS ? xyzf1 : xyzf0));
+		GSVector4i p1 = xy1.blend16<0xf0>(z1.uph32(xyzf1));
+
+		pmin = pmin.min_u32(p0.min_u32(p1));
+		pmax = pmax.max_u32(p0.max_u32(p1));
+
+		#else
+
+		GSVector4 p0 = GSVector4(xy0.upl64(z0.srl32(1).upl32(primclass == GS_SPRITE_CLASS ? xyzf1.wwww() : xyzf0.wwww())));
+		GSVector4 p1 = GSVector4(xy1.upl64(z1.srl32(1).upl32(xyzf1.wwww())));
+
+		pmin = pmin.min(p0.min(p1));
+		pmax = pmax.max(p0.max(p1));
+
+		#endif
+	}
+
+	if ((v_count & 1) && (primclass != GS_SPRITE_CLASS)) // Last item from unrolled loop
+	{
+		const GSVertex& last = v[v_count - 1];
+		GSVector4i c(last.m[0]);
+
+		if (color)
+		{
+			// !iip needs indices on non-point, so do it separately
+			if (iip || primclass == GS_POINT_CLASS)
 			{
 				cmin = cmin.min_u8(c);
 				cmax = cmax.max_u8(c);
 			}
-
-			if(tme)
-			{
-				if(!fst)
-				{
-					GSVector4 stq = GSVector4::cast(c);
-
-					GSVector4 q = stq.wwww();
-
-					if (accurate_stq)
-						stq = (stq.xyww() / q).noopt().xyww(q);
-					else
-						stq = (stq.xyww() * q.rcpnr()).noopt().xyww(q);
-
-					tmin = tmin.min(stq);
-					tmax = tmax.max(stq);
-				}
-				else
-				{
-					GSVector4i uv(v[index[i]].m[1]);
-
-					GSVector4 st = GSVector4(uv.uph16()).xyxy();
-
-					tmin = tmin.min(st);
-					tmax = tmax.max(st);
-				}
-			}
-
-			GSVector4i xyzf(v[index[i]].m[1]);
-
-			GSVector4i xy = xyzf.upl16();
-			GSVector4i z = xyzf.yyyy();
-
-			#if _M_SSE >= 0x401
-
-			GSVector4i p = xy.blend16<0xf0>(z.uph32(xyzf));
-
-			pmin = pmin.min_u32(p);
-			pmax = pmax.max_u32(p);
-
-			#else
-
-			GSVector4 p = GSVector4(xy.upl64(z.srl32(1).upl32(xyzf.wwww())));
-
-			pmin = pmin.min(p);
-			pmax = pmax.max(p);
-
-			#endif
 		}
-		else if(primclass == GS_LINE_CLASS)
+
+		if (tme)
 		{
-			GSVector4i c0(v[index[i + 0]].m[0]);
-			GSVector4i c1(v[index[i + 1]].m[0]);
-
-			if(color)
+			if (!fst)
 			{
-				if(iip)
-				{
-					cmin = cmin.min_u8(c0.min_u8(c1));
-					cmax = cmax.max_u8(c0.max_u8(c1));
-				}
-				else
-				{
-					cmin = cmin.min_u8(c1);
-					cmax = cmax.max_u8(c1);
-				}
-			}
+				GSVector4 stq = GSVector4::cast(c);
 
-			if(tme)
+				GSVector4 q = stq.wwww();
+
+				if (accurate_stq)
+					stq = (stq.xyww() / q).noopt().xyww(q);
+				else
+					stq = (stq.xyww() * q.rcpnr()).noopt().xyww(q);
+
+				tmin = tmin.min(stq);
+				tmax = tmax.max(stq);
+			}
+			else
 			{
-				if(!fst)
-				{
-					GSVector4 stq0 = GSVector4::cast(c0);
-					GSVector4 stq1 = GSVector4::cast(c1);
+				GSVector4i uv(last.m[1]);
 
-					GSVector4 q = stq0.wwww(stq1);
+				GSVector4 st = GSVector4(uv.uph16()).xyxy();
 
-					if(accurate_stq)
-					{
-						GSVector4 st = stq0.xyxy(stq1) / q;
-
-						stq0 = st.xyww(stq1);
-						stq1 = st.zwww(stq1);
-					}
-					else
-					{
-						GSVector4 st = stq0.xyxy(stq1) * q.rcpnr();
-
-						stq0 = st.xyww(stq0);
-						stq1 = st.zwww(stq1);
-					}
-
-					tmin = tmin.min(stq0.min(stq1));
-					tmax = tmax.max(stq0.max(stq1));
-				}
-				else
-				{
-					GSVector4i uv0(v[index[i + 0]].m[1]);
-					GSVector4i uv1(v[index[i + 1]].m[1]);
-
-					GSVector4 st0 = GSVector4(uv0.uph16()).xyxy();
-					GSVector4 st1 = GSVector4(uv1.uph16()).xyxy();
-
-					tmin = tmin.min(st0.min(st1));
-					tmax = tmax.max(st0.max(st1));
-				}
+				tmin = tmin.min(st);
+				tmax = tmax.max(st);
 			}
-
-			GSVector4i xyzf0(v[index[i + 0]].m[1]);
-			GSVector4i xyzf1(v[index[i + 1]].m[1]);
-
-			GSVector4i xy0 = xyzf0.upl16();
-			GSVector4i z0 = xyzf0.yyyy();
-			GSVector4i xy1 = xyzf1.upl16();
-			GSVector4i z1 = xyzf1.yyyy();
-
-			#if _M_SSE >= 0x401
-
-			GSVector4i p0 = xy0.blend16<0xf0>(z0.uph32(xyzf0));
-			GSVector4i p1 = xy1.blend16<0xf0>(z1.uph32(xyzf1));
-
-			pmin = pmin.min_u32(p0.min_u32(p1));
-			pmax = pmax.max_u32(p0.max_u32(p1));
-
-			#else
-
-			GSVector4 p0 = GSVector4(xy0.upl64(z0.srl32(1).upl32(xyzf0.wwww())));
-			GSVector4 p1 = GSVector4(xy1.upl64(z1.srl32(1).upl32(xyzf1.wwww())));
-
-			pmin = pmin.min(p0.min(p1));
-			pmax = pmax.max(p0.max(p1));
-
-			#endif
 		}
-		else if(primclass == GS_TRIANGLE_CLASS)
+
+		GSVector4i xyzf(last.m[1]);
+
+		GSVector4i xy = xyzf.upl16();
+		GSVector4i z = xyzf.yyyy();
+
+		#if _M_SSE >= 0x401
+
+		GSVector4i p = xy.blend16<0xf0>(z.uph32(xyzf));
+
+		pmin = pmin.min_u32(p);
+		pmax = pmax.max_u32(p);
+
+		#else
+
+		GSVector4 p = GSVector4(xy.upl64(z.srl32(1).upl32(xyzf.wwww())));
+
+		pmin = pmin.min(p);
+		pmax = pmax.max(p);
+
+		#endif
+	}
+
+	if (color && !iip && primclass != GS_POINT_CLASS && primclass != GS_SPRITE_CLASS)
+	{
+		int i = n - 1; // Only min/max final vertices
+		for (; i < (i_count - n); i += n * 2) // 2x loop unroll
 		{
-			GSVector4i c0(v[index[i + 0]].m[0]);
-			GSVector4i c1(v[index[i + 1]].m[0]);
-			GSVector4i c2(v[index[i + 2]].m[0]);
-
-			if(color)
-			{
-				if(iip)
-				{
-					cmin = cmin.min_u8(c2).min_u8(c0.min_u8(c1));
-					cmax = cmax.max_u8(c2).max_u8(c0.max_u8(c1));
-				}
-				else
-				{
-					cmin = cmin.min_u8(c2);
-					cmax = cmax.max_u8(c2);
-				}
-			}
-
-			if(tme)
-			{
-				if(!fst)
-				{
-					GSVector4 stq0 = GSVector4::cast(c0);
-					GSVector4 stq1 = GSVector4::cast(c1);
-					GSVector4 stq2 = GSVector4::cast(c2);
-
-					if(accurate_stq)
-					{
-						GSVector4 st01 = stq0.xyxy(stq1) / stq0.wwww(stq1);
-
-						stq0 = st01.xyww(stq0);
-						stq1 = st01.zwww(stq1);
-						stq2 = (stq2.xyww() / stq2.wwww()).noopt().xyww(stq2);
-					}
-					else
-					{
-						GSVector4 q = stq0.wwww(stq1).xzww(stq2).rcpnr();
-						GSVector4 st01 = stq0.xyxy(stq1) * q.xxyy();
-
-						stq0 = st01.xyww(stq0);
-						stq1 = st01.zwww(stq1);
-						stq2 = (stq2.xyww() * q.zzzz()).noopt().xyww(stq2);
-					}
-
-					tmin = tmin.min(stq2).min(stq0.min(stq1));
-					tmax = tmax.max(stq2).max(stq0.max(stq1));
-				}
-				else
-				{
-					GSVector4i uv0(v[index[i + 0]].m[1]);
-					GSVector4i uv1(v[index[i + 1]].m[1]);
-					GSVector4i uv2(v[index[i + 2]].m[1]);
-
-					GSVector4 st0 = GSVector4(uv0.uph16()).xyxy();
-					GSVector4 st1 = GSVector4(uv1.uph16()).xyxy();
-					GSVector4 st2 = GSVector4(uv2.uph16()).xyxy();
-
-					tmin = tmin.min(st2).min(st0.min(st1));
-					tmax = tmax.max(st2).max(st0.max(st1));
-				}
-			}
-
-			GSVector4i xyzf0(v[index[i + 0]].m[1]);
-			GSVector4i xyzf1(v[index[i + 1]].m[1]);
-			GSVector4i xyzf2(v[index[i + 2]].m[1]);
-
-			GSVector4i xy0 = xyzf0.upl16();
-			GSVector4i z0 = xyzf0.yyyy();
-			GSVector4i xy1 = xyzf1.upl16();
-			GSVector4i z1 = xyzf1.yyyy();
-			GSVector4i xy2 = xyzf2.upl16();
-			GSVector4i z2 = xyzf2.yyyy();
-
-			#if _M_SSE >= 0x401
-
-			GSVector4i p0 = xy0.blend16<0xf0>(z0.uph32(xyzf0));
-			GSVector4i p1 = xy1.blend16<0xf0>(z1.uph32(xyzf1));
-			GSVector4i p2 = xy2.blend16<0xf0>(z2.uph32(xyzf2));
-
-			pmin = pmin.min_u32(p2).min_u32(p0.min_u32(p1));
-			pmax = pmax.max_u32(p2).max_u32(p0.max_u32(p1));
-
-			#else
-
-			GSVector4 p0 = GSVector4(xy0.upl64(z0.srl32(1).upl32(xyzf0.wwww())));
-			GSVector4 p1 = GSVector4(xy1.upl64(z1.srl32(1).upl32(xyzf1.wwww())));
-			GSVector4 p2 = GSVector4(xy2.upl64(z2.srl32(1).upl32(xyzf2.wwww())));
-
-			pmin = pmin.min(p2).min(p0.min(p1));
-			pmax = pmax.max(p2).max(p0.max(p1));
-
-			#endif
+			GSVector4i c0 = GSVector4i::load(v[index[i + 0]].RGBAQ.u32[0]);
+			GSVector4i c1 = GSVector4i::load(v[index[i + n]].RGBAQ.u32[0]);
+			cmin = cmin.min_u8(c0.min_u8(c1));
+			cmax = cmax.max_u8(c0.max_u8(c1));
 		}
-		else if(primclass == GS_SPRITE_CLASS)
+		if (i < i_count) // Last item from unrolled loop
 		{
-			GSVector4i c0(v[index[i + 0]].m[0]);
-			GSVector4i c1(v[index[i + 1]].m[0]);
-
-			if(color)
-			{
-				if(iip)
-				{
-					cmin = cmin.min_u8(c0.min_u8(c1));
-					cmax = cmax.max_u8(c0.max_u8(c1));
-				}
-				else
-				{
-					cmin = cmin.min_u8(c1);
-					cmax = cmax.max_u8(c1);
-				}
-			}
-
-			if(tme)
-			{
-				if(!fst)
-				{
-					GSVector4 stq0 = GSVector4::cast(c0);
-					GSVector4 stq1 = GSVector4::cast(c1);
-
-					if(accurate_stq)
-					{
-						GSVector4 st = stq0.xyxy(stq1) / stq1.wwww();
-
-						stq0 = st.xyww(stq1);
-						stq1 = st.zwww(stq1);
-					}
-					else
-					{
-						GSVector4 st = stq0.xyxy(stq1) * stq1.wwww().rcpnr();
-
-						stq0 = st.xyww(stq1);
-						stq1 = st.zwww(stq1);
-					}
-
-					tmin = tmin.min(stq0.min(stq1));
-					tmax = tmax.max(stq0.max(stq1));
-				}
-				else
-				{
-					GSVector4i uv0(v[index[i + 0]].m[1]);
-					GSVector4i uv1(v[index[i + 1]].m[1]);
-
-					GSVector4 st0 = GSVector4(uv0.uph16()).xyxy();
-					GSVector4 st1 = GSVector4(uv1.uph16()).xyxy();
-
-					tmin = tmin.min(st0.min(st1));
-					tmax = tmax.max(st0.max(st1));
-				}
-			}
-
-			GSVector4i xyzf0(v[index[i + 0]].m[1]);
-			GSVector4i xyzf1(v[index[i + 1]].m[1]);
-
-			GSVector4i xy0 = xyzf0.upl16();
-			GSVector4i z0 = xyzf0.yyyy();
-			GSVector4i xy1 = xyzf1.upl16();
-			GSVector4i z1 = xyzf1.yyyy();
-
-			#if _M_SSE >= 0x401
-
-			GSVector4i p0 = xy0.blend16<0xf0>(z0.uph32(xyzf1));
-			GSVector4i p1 = xy1.blend16<0xf0>(z1.uph32(xyzf1));
-
-			pmin = pmin.min_u32(p0.min_u32(p1));
-			pmax = pmax.max_u32(p0.max_u32(p1));
-
-			#else
-
-			GSVector4 p0 = GSVector4(xy0.upl64(z0.srl32(1).upl32(xyzf1.wwww())));
-			GSVector4 p1 = GSVector4(xy1.upl64(z1.srl32(1).upl32(xyzf1.wwww())));
-
-			pmin = pmin.min(p0.min(p1));
-			pmax = pmax.max(p0.max(p1));
-
-			#endif
+			GSVector4i c = GSVector4i::load(v[index[i]].RGBAQ.u32[0]);
+			cmin = cmin.min_u8(c);
+			cmax = cmax.max_u8(c);
 		}
 	}
 
-	// FIXME/WARNING. A division by 2 is done on the depth. I suspect to avoid
+	// FIXME/WARNING. A division by 2 is done on the depth on SSE<4.1 to avoid
 	// negative value. However it means that we lost the lsb bit. m_eq.z could
 	// be true if depth isn't constant but close enough. It also imply that
 	// pmin.z & 1 == 0 and pax.z & 1 == 0
-
-	#if _M_SSE >= 0x401
-
-	pmin = pmin.blend16<0x30>(pmin.srl32(1));
-	pmax = pmax.blend16<0x30>(pmax.srl32(1));
-
-	#endif
 
 	GSVector4 o(context->XYOFFSET);
 	GSVector4 s(1.0f / 16, 1.0f / 16, 2.0f, 1.0f);
 
 	m_min.p = (GSVector4(pmin) - o) * s;
 	m_max.p = (GSVector4(pmax) - o) * s;
+
+	#if _M_SSE >= 0x401
+
+	// Keep full precision on depth
+	m_min.p = m_min.p.insert32<0, 2>(GSVector4::load((float)(uint32)pmin.extract32<2>()));
+	m_max.p = m_max.p.insert32<0, 2>(GSVector4::load((float)(uint32)pmax.extract32<2>()));
+
+	#endif
 
 	if(tme)
 	{
@@ -559,8 +405,8 @@ void GSVertexTrace::FindMinMax(const void* vertex, const uint32* index, int coun
 
 	if(color)
 	{
-		m_min.c = cmin.zzzz().u8to32();
-		m_max.c = cmax.zzzz().u8to32();
+		m_min.c = cmin.u8to32();
+		m_max.c = cmax.u8to32();
 	}
 	else
 	{
