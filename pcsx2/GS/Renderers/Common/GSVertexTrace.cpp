f9da06331ebd5233da919dcfe3aab56f2bc4eecf
TellowKrinkle <tellowkrinkle@gmail.com>
2021-04-19 03:29:59 -0500

    SSE2

--- pcsx2/GS/Renderers/Common/GSVertexTrace.cpp
+++ pcsx2/GS/Renderers/Common/GSVertexTrace.cpp
@@ -174,8 +174,17 @@ void GSVertexTrace::FindMinMax(const void* vertex, const uint32* index, int v_co
 	GSVector4i cmin = GSVector4i::xffffffff();
 	GSVector4i cmax = GSVector4i::zero();
 
+#if _M_SSE >= 0x401
+
 	GSVector4i pmin = GSVector4i::xffffffff();
 	GSVector4i pmax = GSVector4i::zero();
+
+#else
+
+	GSVector4 pmin = s_minmax.xxxx();
+	GSVector4 pmax = s_minmax.yyyy();
+
+#endif
 
 	const GSVertex* RESTRICT v = (GSVertex*)vertex;
 
@@ -247,11 +256,23 @@ void GSVertexTrace::FindMinMax(const void* vertex, const uint32* index, int v_co
 		GSVector4i xy1 = xyzf1.upl16();
 		GSVector4i z1 = xyzf1.yyyy();
 
+#if _M_SSE >= 0x401
+
 		GSVector4i p0 = xy0.blend16<0xf0>(z0.uph32(primclass == GS_SPRITE_CLASS ? xyzf1 : xyzf0));
 		GSVector4i p1 = xy1.blend16<0xf0>(z1.uph32(xyzf1));
 
 		pmin = pmin.min_u32(p0.min_u32(p1));
 		pmax = pmax.max_u32(p0.max_u32(p1));
+
+#else
+
+		GSVector4 p0 = GSVector4(xy0.upl64(z0.srl32(1).upl32(primclass == GS_SPRITE_CLASS ? xyzf1.wwww() : xyzf0.wwww())));
+		GSVector4 p1 = GSVector4(xy1.upl64(z1.srl32(1).upl32(xyzf1.wwww())));
+
+		pmin = pmin.min(p0.min(p1));
+		pmax = pmax.max(p0.max(p1));
+
+#endif
 	};
 
 	for (int i = 0; i < (v_count - 1); i += 2) // 2x loop unroll
@@ -278,15 +299,24 @@ void GSVertexTrace::FindMinMax(const void* vertex, const uint32* index, int v_co
 		}
 	}
 
+	// FIXME/WARNING. A division by 2 is done on the depth on SSE<4.1 to avoid
+	// negative value. However it means that we lost the lsb bit. m_eq.z could
+	// be true if depth isn't constant but close enough. It also imply that
+	// pmin.z & 1 == 0 and pax.z & 1 == 0
+
 	GSVector4 o(context->XYOFFSET);
 	GSVector4 s(1.0f / 16, 1.0f / 16, 2.0f, 1.0f);
 
 	m_min.p = (GSVector4(pmin) - o) * s;
 	m_max.p = (GSVector4(pmax) - o) * s;
 
-	// Fix signed int conversion
+#if _M_SSE >= 0x401
+
+	// Keep full precision on depth
 	m_min.p = m_min.p.insert32<0, 2>(GSVector4::load((float)(uint32)pmin.extract32<2>()));
 	m_max.p = m_max.p.insert32<0, 2>(GSVector4::load((float)(uint32)pmax.extract32<2>()));
+
+#endif
 
 	if (tme)
 	{
